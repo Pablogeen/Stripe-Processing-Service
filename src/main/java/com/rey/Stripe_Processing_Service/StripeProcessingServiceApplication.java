a794ed99163967115d5f6a9ea53f1bbf00403510
benandco <tettehbernard283@gmail.com>
2026-02-19 11:13:00 +0000

    Initial Commit

---
+++ src/main/java/com/rey/Stripe_Processing_Service/StripeProcessingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rey.Stripe_Processing_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripeProcessingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StripeProcessingServiceApplication.class, args);
+	}
+
+}
