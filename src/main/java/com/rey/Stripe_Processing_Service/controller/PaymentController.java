e5211304196b0f265f621a917fa1a039924fbd9f
benandco <tettehbernard283@gmail.com>
2026-04-16 10:52:18 +0000

    javax.net.ssl.SSLHandshakeException: Remote host terminated the handshake

--- src/main/java/com/rey/Stripe_Processing_Service/controller/PaymentController.java
+++ src/main/java/com/rey/Stripe_Processing_Service/controller/PaymentController.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/payments/")
@@ -20,11 +17,21 @@ public class PaymentController {
 
         private final ServiceInterface serviceInterface;
 
-        @PostMapping("/create-payment")
+        @PostMapping("create-payment/")
     public ResponseEntity<PaymentResponse> createPayment(@RequestBody CreatePaymentRequest paymentRequest){
         log.info("Request made to create Payment: {}",paymentRequest);
             PaymentResponse paymentResponse = serviceInterface.makePayment(paymentRequest);
         log.info("Payment Response: {}",paymentResponse);
         return new ResponseEntity<>(paymentResponse, HttpStatus.OK);
     }
+
+
+    @PostMapping("{txnReference}/initiate/")
+    public ResponseEntity<PaymentResponse> initiatePayment(@PathVariable String txnReference){
+            log.info("Request made to initiate Payment with txnReference: {}",txnReference);
+            PaymentResponse paymentResponse = serviceInterface.initiatePayment(txnReference);
+            log.info("Payment has been initiated Successfully: {}",paymentResponse);
+            return new ResponseEntity<>(paymentResponse, HttpStatus.OK);
+    }
+
 }
