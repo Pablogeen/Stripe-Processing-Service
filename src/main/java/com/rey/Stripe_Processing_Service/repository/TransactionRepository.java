f8ec5feead502034051f235dc67907b51c73b0d8
benandco <tettehbernard283@gmail.com>
2026-04-24 14:39:06 +0000

    Idempotency with Retries

--- src/main/java/com/rey/Stripe_Processing_Service/repository/TransactionRepository.java
+++ src/main/java/com/rey/Stripe_Processing_Service/repository/TransactionRepository.java
@@ -1,7 +1,10 @@
 package com.rey.Stripe_Processing_Service.repository;
 
 import com.rey.Stripe_Processing_Service.entity.Transaction;
+import com.rey.Stripe_Processing_Service.entity.TransactionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +14,8 @@ public interface TransactionRepository extends JpaRepository<Transaction, Intege
 
 
     Optional<Transaction> findBytxnReference(String txnReference);
+
+    @Modifying
+    @Query("UPDATE Transaction t SET t.status = :newStatus WHERE t.txnReference = :ref AND t.status = :currentStatus")
+    int updateStatus(String ref, TransactionStatus newStatus, TransactionStatus currentStatus);
 }
